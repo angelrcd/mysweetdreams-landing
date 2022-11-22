e69b4dbbb6bd95e18a7aea800bb62eb8c7280c0d
Angel Romero <mrangelrcd@gmail.com>
2022-11-22 12:05:04 +0100

    Landing first draft copied

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        web: {
+          boton: "#79b5f3",
+          boton2: "#3f67bc",
+          fondo: "#C9D5E0",
+          topBar: "#4FA9A9",
+          loginButton: "#A5E1AD",
+          custom1: "#21094E",
+          custom2: "#511281",
+          custom3: "#4CA1A3",
+          custom4: "#A5E1AD"
+        }
+      },
+
+      fontFamily: {
+        bluetea: "bluetea",
+        quicksand: "Quicksand",
+        caveat: "Caveat",
+        urbanist: "Urbanist"
+      }
+    },
+  },
+  plugins: [],
+};
