a3a29143d24198ccfc0da176e3a7a645a13368a8
Angel Romero <mrangelrcd@gmail.com>
2022-12-10 15:18:16 +0100

    no delay minimize bar

--- src/index.js
+++ src/index.js
@@ -40,36 +40,26 @@ function setNavNotVisible() {
 
 function setDesktopNavBackground(scrollYPosition) {
   if (scrollYPosition > 0) {
-    setTimeout(() => {
-      navbar.classList.remove("md:bg-transparent", "md:h-20");
-      navbar.classList.add("md:bg-gray-200", "md:dark:bg-web-formBgDarkMode", "md:border-gray-400");
-      navTitle.classList.add("md:text-xl", "text-gray-900", "dark:text-gray-200");
-      navTitle.classList.remove("text-shadow-1", "lg:text-shadow-2");
-      logo.classList.remove("md:h-[70px]");
-      navButtons.forEach(navButton => {
-        navButton.classList.add("text-gray-900", "dark:text-gray-200", "text-lg");
-        navButton.classList.remove("text-shadow-1");
-      });
-    }, 300);
+    navbar.classList.remove("md:bg-transparent", "md:h-20");
+    navbar.classList.add("md:bg-gray-200", "md:dark:bg-web-formBgDarkMode", "md:border-gray-400");
+    navTitle.classList.add("md:text-xl", "text-gray-900", "dark:text-gray-200");
+    navTitle.classList.remove("text-shadow-1", "lg:text-shadow-2");
+    logo.classList.remove("md:h-[70px]");
+    navButtons.forEach(navButton => {
+      navButton.classList.add("text-gray-900", "dark:text-gray-200", "text-lg");
+      navButton.classList.remove("text-shadow-1");
+    });
   } else {
-    setTimeout(() => {
-      navbar.classList.add("md:bg-transparent");
-      navbar.classList.remove("md:bg-gray-200");
-      navbar.classList.remove("md:dark:bg-web-formBgDarkMode");
-      navbar.classList.remove("md:border-gray-400");
-      navbar.classList.remove("md:dark:border-gray-400");
-      navbar.classList.add("duration-300");
-      navbar.classList.add("md:h-20");
-      navTitle.classList.remove("md:text-xl", "text-gray-900", "dark:text-gray-200");
-      navTitle.classList.add("text-shadow-1", "lg:text-shadow-2");
-      logo.classList.add("md:h-[70px]");
-      navButtons.forEach(navButton => {
-        navButton.classList.remove("text-gray-900");
-        navButton.classList.remove("dark:text-gray-200");
-        navButton.classList.add("text-shadow-1");
-        navButton.classList.remove("text-lg");
-      });
-    }, 300);
+    navbar.classList.add("duration-300");
+    navbar.classList.add("md:bg-transparent", "md:h-20");
+    navbar.classList.remove("md:bg-gray-200", "md:dark:bg-web-formBgDarkMode", "md:border-gray-400");
+    navTitle.classList.remove("md:text-xl", "text-gray-900", "dark:text-gray-200");
+    navTitle.classList.add("text-shadow-1", "lg:text-shadow-2");
+    logo.classList.add("md:h-[70px]");
+    navButtons.forEach(navButton => {
+      navButton.classList.remove("text-gray-900", "dark:text-gray-200", "text-lg");
+      navButton.classList.add("text-shadow-1");
+    });
   }
 }
 
